a533d7444c0a7260a840d3243efef0473aa66fe9
Ana Oliveira <oliveirabrancoana@gmail.com>
2022-05-10 10:17:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wello World!");
+            Console.WriteLine(" ou ");
+            Console.WriteLine("Ola Mundo!");
+        }
+    }
+}
